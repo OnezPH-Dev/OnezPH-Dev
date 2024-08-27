65b597d634ef17ab9222acd57790769af80fe2d2
Wilzen Carl R. Tolentino <157259745+OnezPH-Dev@users.noreply.github.com>
2024-08-27 00:05:23 -0700

    onez

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+AOS.init();
+var bodyHeight = document.querySelector('.mainCon').scrollHeight,
+morphi = document.getElementById("morpCon"),
+design = document.getElementById("design");
 
+morphi.style.height = bodyHeight + "px";
+design.style.height = bodyHeight + "px";
+
+console.log(bodyHeight)
+
+window.addEventListener('scroll', reveal)
+function reveal(){
+    if(window.scrollY >= 80){
+        document.getElementById('navbar').classList.add('navbarActive');
+    }else{
+        document.getElementById('navbar').classList.remove('navbarActive');
+     }
+}
